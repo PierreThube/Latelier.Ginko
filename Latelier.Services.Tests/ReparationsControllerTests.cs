9a920bf87a11a55a4f14696c56c9491584472438
PierreThube <thube.pierre@gmail.com>
2023-10-27 13:38:42 +0200

    Ajout d'un premier test basique

--- Latelier.Services.Tests/ReparationsControllerTests.cs
+++ Latelier.Services.Tests/ReparationsControllerTests.cs
@@ -1,11 +1,34 @@
+using Latelier.Services.Controllers;
+using Latelier.Services.Services;
+
 namespace Latelier.Services.Tests
 {
     [TestClass]
     public class ReparationsControllerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private int ReparationId = 0;
+
+        public ReparationsControllerTests()
         {
+            Console.WriteLine($"Machine name = {Environment.MachineName}");
+            ReparationId = Environment.MachineName switch
+            {
+                "PIERRE-PC" => 1,
+                _ => 0
+            };
+        }
+
+        [TestMethod]
+        public void SearchReparationById_ShouldFind()
+        {
+            var reparation = DataServices.Get(ReparationId);
+            if (reparation == null)
+                Assert.Inconclusive($"Aucune réparation pour l'id : {ReparationId}");
+
+            var services = new ReparationsController();
+            var result = services.SearchReparationById(ReparationId);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result?.Value?.Id, ReparationId);
         }
     }
 }
